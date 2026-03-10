8532d2a7f9cfed0ae8beb3aab42d2b895c690710
GabrielYYM <gabrielyyminato@gmail.com>
2026-03-10 05:07:39 -0300

    Add: front end

---
+++ src/main/java/com/repositorio/mvp/controller/WebController.java
@@ -0,0 +1,22 @@
+package com.repositorio.mvp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+import com.repositorio.mvp.service.UserService;
+
+@Controller
+@RequiredArgsConstructor
+public class WebController {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public String usersPage(Model model) {
+        model.addAttribute("users", userService.listAllUsers());
+        return "index";
+    }
+}

f4824c59d9a2ac4ddc2774df8e648ebe93303958
GabrielYYM <gabrielyyminato@gmail.com>
2026-03-10 06:06:16 -0300

    chore: diseble formlog

--- src/main/java/com/repositorio/mvp/config/SecurityConfig.java
+++ src/main/java/com/repositorio/mvp/config/SecurityConfig.java
@@ -2,14 +2,32 @@ package com.repositorio.mvp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
-
+    
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //remove o form de login e auth
+        http
+            .csrf(csrf -> csrf.disable())
+            .formLogin(form -> form.disable())
+            .httpBasic(httpBasic -> httpBasic.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+    
+    @Bean
+    //hash com salt
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(14);
     }
 }

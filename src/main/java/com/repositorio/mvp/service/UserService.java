ad1aa7c26293e458594df6bfc2634691d4d6a574
GabrielYYM <gabrielyyminato@gmail.com>
2026-03-10 05:38:38 -0300

    Add: BCrypt

--- src/main/java/com/repositorio/mvp/service/UserService.java
+++ src/main/java/com/repositorio/mvp/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.repositorio.mvp.DTO.user.UserRequestDTO;
 import com.repositorio.mvp.DTO.user.UserResponseDTO;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     
     private UserResponseDTO toUserResponseDTO(User user){
         return new UserResponseDTO(
@@ -30,11 +32,13 @@ public class UserService {
 
     //Metodo para criar um novo usuário
     public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
+        String hashedPassword = passwordEncoder.encode(userRequestDTO.password());
+
         //Usamos o Builder para transformar o DTO de entrada na Entidade
         User user = User.builder()
             .name(userRequestDTO.name())
             .email(userRequestDTO.email())
-            .password(userRequestDTO.password())
+            .password(hashedPassword)
             .build();
         User savedUser = userRepository.save(user);
 
@@ -70,7 +74,7 @@ public class UserService {
         // Atualiza a entidade existente com os dados novos do DTO
         userToUpdate.setName(updatedUser.name());
         userToUpdate.setEmail(updatedUser.email());
-        userToUpdate.setPassword(updatedUser.password());
+        userToUpdate.setPassword(passwordEncoder.encode(updatedUser.password()));
 
         User savedUser = userRepository.save(userToUpdate);
         return toUserResponseDTO(savedUser);

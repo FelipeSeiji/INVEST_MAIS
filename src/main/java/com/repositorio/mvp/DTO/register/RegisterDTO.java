eee33f14af63d41617bded42206375fbd32e532d
GabrielYYM <gabrielyyminato@gmail.com>
2026-03-12 12:02:14 -0300

    Remove: UserRole from DTO

--- src/main/java/com/repositorio/mvp/DTO/register/RegisterDTO.java
+++ src/main/java/com/repositorio/mvp/DTO/register/RegisterDTO.java
@@ -1,8 +1,6 @@
 package com.repositorio.mvp.DTO.register;
 
-import com.repositorio.mvp.model.UserRole;
-
-public record RegisterDTO(String name, String email, String password, UserRole role) {
+public record RegisterDTO(String name, String email, String password) {
 
 
 }

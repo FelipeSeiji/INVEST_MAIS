a0e312114bbccd1e07d72fcdb241ff6281d10cf3
GabrielYYM <gabrielyyminato@gmail.com>
2026-03-12 11:44:40 -0300

    Remove: unused imports

--- src/main/java/com/repositorio/mvp/config/SecurityConfig.java
+++ src/main/java/com/repositorio/mvp/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.repositorio.mvp.config;
 
-import java.security.Security;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/repositorio/mvp/service/TokenService.java
+++ src/main/java/com/repositorio/mvp/service/TokenService.java
@@ -7,8 +7,6 @@ import java.time.ZoneOffset;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.repositorio.mvp.model.User;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
